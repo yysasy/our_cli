becd2b1c91d3b783f08896b3ac66c4ef5de18bfd
yysasy <yysasy@yeah.net>
2024-11-04 11:56:28 +0800

    更新

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Command } from 'commander'
 import { version } from '../package.json'
 import { create } from './command/create'
-const program = new Command('yy_cli');
+const program = new Command('kuka_cli');
 program.version(version,'-v --version');
 
 program.command('create').description('创建一个项目').argument('[name]','项目名称').action(async (dirName) => {
